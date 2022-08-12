48aa77f6d896d2c6daa243ecc9b2739068e268ae
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-08-12 23:12:05 +0800

    YOU ASKED FOR IT GORODMI
    **YOU ASKED FOR IT**

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 function veryLoud(){
-    Core.audio.soundBus.setVolume(100);
-    Core.audio.musicBus.setVolume(100);
+    Core.audio.soundBus.setVolume(1000);
+    Core.audio.musicBus.setVolume(1000);
 }
 Events.on(ClientLoadEvent, e => veryLoud());
 Events.on(WorldLoadEvent, e => {
